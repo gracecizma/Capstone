8771482e59451cac3450fbae7028ef4b5f288930
gracecizma <gracecizma@gmail.com>
2023-03-22 15:03:21 -0500

    product routes

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,4 @@
-from .db import db, environment, SCHEMA
+from .db import db, environment, SCHEMA, add_prefix_for_prod
 from .user import User
 from .favorites import Favorite
 from .order_products import OrderProduct
