141ba994025051abbc2e3960d3057cd20fb83279
gracecizma <gracecizma@gmail.com>
2023-03-23 18:36:48 -0500

    style update

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -15,33 +15,35 @@ function Navigation({ isLoaded }) {
 		<>
 			<div className="nav-bar">
 				<div className="nav-links">
-					<div className="header">
-						<NavLink exact to="/" className="logo-text">Gracefully Baked</NavLink>
-					</div>
 					<div className="menu">
 						<NavLink exact to="/products" className="menu-link">Menu</NavLink>
 					</div>
 				</div>
-				<div className="search-container">
-					<div className="search-form">
-						<form
-							className="search-form"
-							onSubmit={() => history.push(`/search/${search}`)}
-						>
-							<input
-								placeholder="Search for something"
-								className="search-form-input"
-								onChange={(e) => setSearch(e.target.value)}
-							/>
-						</form>
-					</div>
+				<div className="header">
+					<NavLink exact to="/" className="logo-text">Gracefully Baked</NavLink>
 				</div>
+
 				{isLoaded && (
 					<div className="nav-bar-buttons">
 
+						<div className="search-container">
+							<div className="search-form">
+								<form
+									className="search-form"
+									onSubmit={() => history.push(`/search/${search}`)}
+								>
+									<input
+										placeholder="Search for something"
+										className="search-form-input"
+										onChange={(e) => setSearch(e.target.value)}
+									/>
+								</form>
+							</div>
+						</div>
 						<div className="user-menu-button">
 							<ProfileButton user={sessionUser} />
 						</div>
+
 					</div>
 				)}
 			</div>
