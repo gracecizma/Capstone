699ba99ce612be9651afdb8043f85f65c82dd7f7
gracecizma <gracecizma@gmail.com>
2023-03-27 10:56:39 -0500

    update to total reviews

--- react-app/src/components/Products/AllProducts.js
+++ react-app/src/components/Products/AllProducts.js
@@ -39,8 +39,9 @@ export default function Products() {
                   Rating:{product?.avg_rating ? ' ★ ' + Number(product?.avg_rating).toFixed(1) : '★ New'}
                 </p>
                 <p className="product-total-reviews">
+                  {product?.total_reviews === 0 ? "" : ""}
                   {product?.total_reviews === 1 ? product?.total_reviews + ' review' : ""}
-                  {product?.total_reviews !== 1 ? product?.total_reviews + ' reviews' : ""}
+                  {product?.total_reviews > 1 ? product?.total_reviews + ' reviews' : ""}
                 </p>
                 <p className="product-price">
                   ${product?.price}
