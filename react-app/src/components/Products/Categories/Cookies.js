97f7ca335699d005937d0aa24c07f089f2a50d12
gracecizma <gracecizma@gmail.com>
2023-03-31 12:50:32 -0500

    styling updated

--- react-app/src/components/Products/Categories/Cookies.js
+++ react-app/src/components/Products/Categories/Cookies.js
@@ -2,7 +2,10 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom"
 import { getAllProducts } from "../../../store/products";
-
+import AddToCart from "../../ShoppingCart/AddToCart";
+import OpenModalMenuItem from "../../OpenModalButton/MenuItem";
+import { addItemToCart } from "../../../store/shoppingcart";
+import '../allproducts.css'
 
 export default function Cookies() {
   const dispatch = useDispatch()
@@ -10,6 +13,7 @@ export default function Cookies() {
   console.log("products obj", products)
   const productsArr = Object.values(products)
   console.log("products array", productsArr)
+  const currUser = useSelector((state) => state?.session?.user)
 
   if (!productsArr.length) dispatch(getAllProducts())
 
@@ -20,6 +24,11 @@ export default function Cookies() {
     }
   })
 
+  const disableButton = () => {
+    if (!currUser) {
+      return true;
+    }
+  };
 
 
 
@@ -28,6 +37,11 @@ export default function Cookies() {
       <div className="products-div">
         {cookieArr.map(product => (
           <Link key={product?.id} to={`/products/${product?.id}`} className="product-tile">
+            <div className="product-name-container">
+              <p className="product-name">
+                {product?.name}
+              </p>
+            </div>
             <div className="product-img-container">
               <img
                 className="product-img"
@@ -35,10 +49,7 @@ export default function Cookies() {
             </div>
 
             <div className="product-details-container">
-              <div className="product-name-rating-price">
-                <p className="product-name">
-                  {product?.name}
-                </p>
+              <div className="product-rating-container">
                 <p className="product-rating">
                   Rating:{product?.avg_rating ? ' ★ ' + Number(product?.avg_rating).toFixed(1) : '★ New'}
                 </p>
@@ -47,9 +58,31 @@ export default function Cookies() {
                   {product?.total_reviews === 1 ? product?.total_reviews + ' review' : ""}
                   {product?.total_reviews > 1 ? product?.total_reviews + ' reviews' : ""}
                 </p>
+              </div>
+              <div className="price-and-cart-container">
                 <p className="product-price">
-                  ${product?.price}
+                  ${parseFloat(product?.price).toFixed(2)}
                 </p>
+                <button
+                  className="cart-button"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    const data = {
+                      user_id: currUser.id,
+                      product_id: product.id,
+                      quantity: 1
+                    }
+                    console.log("item to be added", data)
+                    dispatch(addItemToCart(data))
+                  }}
+                >
+                  <OpenModalMenuItem
+                    itemText="Add to cart"
+                    itemTextClassName="cart-button-text"
+                    modalDisabled={disableButton}
+                    modalComponent={<AddToCart product={product} quantity={1} />}
+                  />
+                </button>
               </div>
             </div>
           </Link>
